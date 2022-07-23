c30b3d39f24f9e345288269618da5bb75d328f23
panospr2991 <pprovi01@mail.bbk.ac.uk>
2022-07-23 16:09:28 +0100

    add more parameters to config

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 class Config:
     def __init__(self):
+        self.POPULATION = 200
         self.GENOME_LENGTH = 32
+        self.MUTATION_PROBABILITY = 0.002
+        self.ELITE_CARRY_OVER = 5
+        self.IDEAL_FITNESS = 0
         self.WHITE_RATIO = 0.5
         self.UNIFORM_PROBABILITY = 0.5

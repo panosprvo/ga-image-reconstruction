cab8f35fe5a985d630e487ace3768c370ed92ed8
panospr2991 <pprovi01@mail.bbk.ac.uk>
2022-07-24 17:16:34 +0100

    fixed mutation class to accommodate 2D arrays

--- mutation.py
+++ mutation.py
@@ -1,13 +1,17 @@
+import copy
 import random
 
+import numpy as np
+
+from binary_image_generation import BinaryImage
 from config import Config
 
 
-class Chromosome:
+class Mutation:
 
-    def __init__(self):
+    def __init__(self, individual):
         self.config = Config()
-        self.genome = [random.randint(0, 1) for _ in range(self.config.GENOME_LENGTH)]
+        self.genome = individual
 
     def do_mutation(self):
         """
@@ -15,9 +19,11 @@ class Chromosome:
         mutation for the specific gene should take place or not. Mutation is the reversal of 0 to 1, and 1 to 0.
         :return: void - method that only flips genes that should be mutated.
         """
-        for index in range(self.config.GENOME_LENGTH):
-            if random.uniform(0, 1) <= self.config.MUTATION_PROBABILITY:
-                self.genome[index] = self.__mutate(self.genome[index])
+        for j in range(self.config.GENOME_LENGTH):
+            for index in range(self.config.GENOME_LENGTH):
+                if random.uniform(0, 1) <= self.config.MUTATION_PROBABILITY:
+                    self.genome[j][index] = self.__mutate(self.genome[j][index])
+        # print(f"M: {self.genome}")
 
     def __mutate(self, gene):
         if gene == 0:
@@ -26,7 +32,16 @@ class Chromosome:
 
 
 def main():
-    return
+    bi1 = BinaryImage()
+    img1 = bi1.create_random_binary_image()
+    par1 = bi1.binary_image_to_binary_array(img1)
+
+    mut = Mutation(par1)
+    original = copy.deepcopy(mut.genome)
+    print(f"I: {mut.genome}")
+    mut.do_mutation()
+    print(f"M: {mut.genome}")
+    print(np.array_equal(mut.genome, original))
 
 
 if __name__ == '__main__':

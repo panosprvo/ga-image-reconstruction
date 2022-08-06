ec64764418e1fe3a93d866a64ff0b3953898eac6
panospr2991 <pprovi01@mail.bbk.ac.uk>
2022-08-06 15:16:13 +0100

    introduced the function of genotype to represent each individual to avoid issues with the algorithm operators

---
+++ genotype.py
@@ -0,0 +1,36 @@
+import random
+
+from config import Config
+
+
+class Chromosome:
+
+    def __init__(self):
+        self.config = Config()
+        self.genes = [[random.randint(0, 1) for i in range(0, self.config.GENOME_LENGTH)]
+                      for i in range(0, self.config.GENOME_LENGTH)]
+        self.fitness = 0
+
+    def mutation(self):
+        """
+        Depending on config.MUTATION_PROBABILITY, we produce a random number between 0 and 1 to decide whether a
+        mutation for the specific gene should take place or not. Mutation is the reversal of 0 to 1, and 1 to 0.
+        :return: void - method that only flips genes that should be mutated.
+        """
+        for j in range(self.config.GENOME_LENGTH):
+            for index in range(self.config.GENOME_LENGTH):
+                if random.uniform(0, 1) <= self.config.MUTATION_PROBABILITY:
+                    self.genes[j][index] = self.__mutate__(self.genes[j][index])
+
+    def __mutate__(self, gene):
+        if gene == 0:
+            return 1
+        return 0
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()

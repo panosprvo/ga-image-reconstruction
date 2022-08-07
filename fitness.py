93257b2f2954771dcb9a6c2f8d740a8b9bbfbaaa
panospr2991 <pprovi01@mail.bbk.ac.uk>
2022-08-07 15:23:30 +0100

    introduce new methods for fitness

--- fitness.py
+++ fitness.py
@@ -7,14 +7,14 @@ from config import Config
 class Fitness:
     def __init__(self):
         self.config = Config()
-        self.fitness = self.config.GENOME_LENGTH * self.config.GENOME_LENGTH
+        self.fitness_to_reach = self.config.GENOME_LENGTH * self.config.GENOME_LENGTH
 
-    def optimal_fitness_array(self):
+    def optimal_fitness_genotype(self):
         """
         This method is used to get the array of the image to be reconstructed. This is the array that will be used to
         compare each individual's array, therefore its fitness, while the algorithm is running.
 
-        :return: a 2d binary array.
+        :return: a Genotype object.
         """
         optimal_array = BinaryImage().open_original_image()
         return optimal_array
@@ -30,7 +30,7 @@ class Fitness:
 
         :return: change an individual's fitness (void method).
         """
-        individual_genotype.fitness = self.fitness
+        individual_genotype.fitness = self.fitness_to_reach
         for i in range(self.config.GENOME_LENGTH):
             for j in range(self.config.GENOME_LENGTH):
                 if optimal_genotype.genes[i][j] == individual_genotype.genes[i][j]:
@@ -38,20 +38,41 @@ class Fitness:
                 else:
                     individual_genotype.fitness -= 1
 
+    def get_max_fitness(self, population):
+        """
+        A method that returns the maximum fitness in a given population.
+
+        :param population: an array of Genotype objects.
+
+        :return: int.
+        """
+        return max(individual.fitness for individual in population)
+
+    def max_fitness_genotype(self, population):
+        """
+        A method that returns the individual with the maximum fitness in a given population.
+
+        :param population: an array of Genotype objects.
+
+        :return: a Genotype object.
+        """
+        population.sort(key=lambda x: x.fitness, reverse=True)
+        return population[0]
+
 
 def main():
     population = InitialGeneration().initialise()
     fitness = Fitness()
-    genes = fitness.optimal_fitness_array()
+    genes = fitness.optimal_fitness_genotype()
     optimal_array = Genotype()
     optimal_array.genes = genes
 
     for i in range(10):
         fitness.evaluate_fitness(optimal_array, population[i])
-        print(population[i].fitness)
-    print(f"Average fitness: {sum(i.fitness for i in population) / 10}")
-    print(f"Max fitness: {max(i.fitness for i in population)}")
-    print(f"Min fitness: {min(i.fitness for i in population)}")
+        print(population[i].fitness_to_reach)
+    print(f"Average fitness: {sum(i.fitness_to_reach for i in population) / 10}")
+    print(f"Max fitness: {max(i.fitness_to_reach for i in population)}")
+    print(f"Min fitness: {min(i.fitness_to_reach for i in population)}")
 
 
 if __name__ == '__main__':
--- testing/fitness_test.py
+++ testing/fitness_test.py
@@ -1,13 +1,48 @@
 from fitness import *
 
 
-def test_evaluate_fitness1():
+def test_evaluate_fitness():
     optimal_genotype = Genotype()
     optimal_genotype.genes = [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [1, 1, 1, 1]]
     individual_genotype = Genotype()
     individual_genotype.genes = [[0, 1, 0, 1], [0, 0, 0, 1], [1, 0, 1, 1], [0, 0, 1, 0]]
     fitness = Fitness()
     fitness.config.GENOME_LENGTH = 4
-    fitness.fitness = 16
+    fitness.fitness_to_reach = 16
     fitness.evaluate_fitness(optimal_genotype, individual_genotype)
     assert individual_genotype.fitness == 5
+
+
+def test_get_max_fitness():
+    gen1 = Genotype()
+    gen1.genes = [[0, 0, 0], [0, 0, 1], [1, 0, 1]]
+    gen1.fitness = 10
+    gen2 = Genotype()
+    gen2.genes = [[0, 0, 0], [0, 0, 1], [1, 0, 1]]
+    gen2.fitness = 15
+    gen3 = Genotype()
+    gen3.genes = [[0, 0, 0], [0, 0, 1], [1, 0, 1]]
+    gen3.fitness = 25
+    population = [gen1, gen2, gen3]
+
+    fitness = Fitness()
+    fitness.config.GENOME_LENGTH = 3
+    assert fitness.get_max_fitness(population) == 25
+
+
+def test_max_fitness_genotype():
+    gen1 = Genotype()
+    gen1.genes = [[0, 0, 0], [0, 0, 0], [1, 0, 0]]
+    gen1.fitness = 10
+    gen2 = Genotype()
+    gen2.genes = [[0, 0, 0], [1, 0, 0], [0, 0, 0]]
+    gen2.fitness = 15
+    gen3 = Genotype()
+    gen3.genes = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    gen3.fitness = 25
+    population = [gen1, gen2, gen3]
+
+    fitness = Fitness()
+    fitness.config.GENOME_LENGTH = 3
+    test_gen = fitness.max_fitness_genotype(population)
+    assert test_gen.genes == gen3.genes

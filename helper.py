b86ffb2997808de5618f9ac119c6e107f1eb332f
panospr2991 <pprovi01@mail.bbk.ac.uk>
2022-08-07 13:26:11 +0100

    changes to helper methods

--- helper.py
+++ helper.py
@@ -4,33 +4,25 @@ import cv2
 import numpy as np
 
 
-def save_binary_array(binary_array):
+def save_binary_array(binary_array, index):
     """
     Helper method that saves a binary array in a txt file.
 
+    :param index: int. Used to number an array that's being saved.
     :param binary_array: a 2d array.
 
     :return: txt file.
     """
-    for i in range(100):
-        if os.path.exists(f"binary_array_{str(i)}.txt"):
-            continue
-        else:
-            np.savetxt(f"binary_array_{str(i)}.txt", binary_array, fmt='%d')
-            break
+    np.savetxt(f"binary_array_{str(index)}.txt", binary_array, fmt='%d')
 
 
-def save_binary_image(img):
+def save_binary_image(img, index):
     """
     Helper method that saves a binary image as a png file.
 
+    :param index: int. Used to number an image that's being saved.
     :param img: a binary image.
 
     :return: png file.
     """
-    for i in range(100):
-        if os.path.exists(f"binary_image_{str(i)}.png"):
-            continue
-        else:
-            cv2.imwrite(f"binary_image_{str(i)}.png", img)
-            break
+    cv2.imwrite(f"binary_image_{str(index)}.png", img)

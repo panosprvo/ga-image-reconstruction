b282c3c80a5f469d5fb68c2f069d37bc4af37448
panospr2991 <pprovi01@mail.bbk.ac.uk>
2022-08-13 14:42:14 +0100

    added signature at top of file

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+# --------------------------------------------
+# 2022, Panagiotis Provias, London, U.K.
+# email: panayiotisprovias@gmail.com
+# --------------------------------------------
+
 class Config:
     def __init__(self):
-        self.GENERATIONS = 500
-        self.POPULATION = 10
+        self.GENERATIONS = 1000
+        self.POPULATION = 300
         self.GENOME_LENGTH = 32
         self.MUTATION_PROBABILITY = 0.002
-        self.ELITE_CARRY_OVER = 5
-        self.IDEAL_FITNESS = 0
+        self.ELITE_CARRY_OVER = int(self.POPULATION * 0.04)
         self.WHITE_RATIO = 0.5
         self.UNIFORM_PROBABILITY = 0.5

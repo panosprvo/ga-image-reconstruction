9ffa07381ab4b6cdd9a9de3e34edce674c1aedad
panospr2991 <pprovi01@mail.bbk.ac.uk>
2022-08-21 18:45:52 +0100

    finalized configuration parameter

--- config.py
+++ config.py
@@ -6,9 +6,9 @@
 class Config:
     def __init__(self):
         self.GENERATIONS = 1000
-        self.POPULATION = 300
+        self.POPULATION = 200
         self.GENOME_LENGTH = 32
         self.MUTATION_PROBABILITY = 0.002
-        self.ELITE_CARRY_OVER = int(self.POPULATION * 0.04)
+        self.ELITE_CARRY_OVER = int(self.POPULATION * 0.02)
         self.WHITE_RATIO = 0.5
         self.UNIFORM_PROBABILITY = 0.5
